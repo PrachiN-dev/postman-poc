de76bcaf57efb518d77337d50e407806a2c76cee
Prachi Narkhede <pnarkhede@claimlogik.com>
2021-09-16 18:32:15 +1000

    Updated index and yml files

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const newman = require("newman");
-// import {run} from 'newman'
-// import integrationCollection from './tests/repair_poc.postman_collection.json';
-// import testEnvironment from './environments/Test.postman_environment.json';
-
-// const collections = [integrationCollection];
-// for(const collection of collections){
 newman.run({
     collection: './tests/repair_poc.postman_collection.json',
     reporters: 'junitfull',
     reporter: {
         junitfull: {
-            export: './reports/result.xml', // If not specified, the file will be written to `newman/` in the current working directory.
+            export: './reports/api-test.xml',
         }
     },
 }, (error) => {
-        if(error) {
-            console.log(error);
-        }
-    // }, (error) => {
-    //     if(error) {
-    //         console.log(error);
-    //     }
+    if(error) {
+        console.log(error);
+    }
 });
-// }

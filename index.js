bbf8f47dcc561b65b84603c60a364dbd157f588d
Prachi Narkhede <pnarkhede@claimlogik.com>
2021-09-29 10:21:10 +1000

    Try html report

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const newman = require('newman');
+require('dotenv').config();
+
 newman.run({
     collection : './tests/repair_poc.postman_collection.json',
-    environment : './environments/Test.postman_environment.json',
+    // environment : './environments/Test.postman_environment.json',
+    envVar: [
+        {"key":"url", "value":'https://reqres.in'},
+        {"key":"userId", "value":`${process.env.USER_ID}` }
+    ],
     reporters: ['junitfull', 'cli'],
     reporter: {
         junitfull: {
